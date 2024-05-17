c10f17ffdd25424e3754d2273a519829d9987af7
mrczkk <kmroczek16@gmail.com>
2024-05-17 14:21:57 +0200

    simple navigation

--- src/components/app-components/NavigationBar.tsx
+++ src/components/app-components/NavigationBar.tsx
@@ -1,16 +1,28 @@
-import { Menubar, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+
 import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
   return (
-    <Menubar>
-      <MenubarMenu>
-        <MenubarTrigger className="cursor-pointer">
-          <Link to={"/events"}>Events</Link>
-          <Link to={"/new-event"}>Add new event</Link>
-        </MenubarTrigger>
-      </MenubarMenu>
-    </Menubar>
+    <NavigationMenu className="bg-primary text-secondary px-4 py-2 w-full rounded-lg mb-12">
+      <NavigationMenuList className="gap-x-4">
+        <NavigationMenuItem>
+          <NavigationMenuLink>
+            <Link to={"/events"}>Events</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink>
+            <Link to={"/new-event"}>Add new event</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
   );
 };
 
