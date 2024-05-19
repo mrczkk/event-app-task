57df76ee9b772c38996cda3961df8d7ff8673a4c
mrczkk <kmroczek16@gmail.com>
2024-05-19 11:21:50 +0200

    example look of event details

--- src/routes/EventsView.tsx
+++ src/routes/EventsView.tsx
@@ -1,7 +1,14 @@
 import EventItem from "@/components/app-components/EventItem";
 
 const EventsView = () => {
-  return <EventItem />;
+  return (
+    <section>
+      <ul className="list-none flex flex-col gap-y-4">
+        <EventItem />
+        <EventItem />
+      </ul>
+    </section>
+  );
 };
 
 export default EventsView;
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -2,12 +2,12 @@ import { Outlet } from "react-router-dom";
 import NavigationBar from "@/components/app-components/NavigationBar";
 const Root = () => {
   return (
-    <>
+    <div className="p-8">
       <NavigationBar />
-      <main>
+      <main className="py-8">
         <Outlet />
       </main>
-    </>
+    </div>
   );
 };
 
