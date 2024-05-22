b0708376888dec464746684743a024d75a620e46
mrczkk <kmroczek16@gmail.com>
2024-05-22 14:31:13 +0200

    centering not found text

--- src/components/app-components/EventsList/EventsList.tsx
+++ src/components/app-components/EventsList/EventsList.tsx
@@ -4,7 +4,8 @@ import EventItem from "./EventItem";
 const EventsList = ({ events }: { events: EventType[] }) => {
   return (
     <ul className="list-none flex flex-col gap-y-4">
-      {!events || (!events.length && <p>No events found</p>)}
+      {!events ||
+        (!events.length && <p className="text-center">No events found</p>)}
       {events.map((event) => (
         <EventItem key={event.id} event={event} />
       ))}
